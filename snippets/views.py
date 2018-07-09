d9188b51b39318332217d2c58e7eb145c12b9d81
dongxi346 <1105894953@qq.com>
2018-07-09 14:06:34 +0800

    使用 ViewSets＆Routers 重构

--- snippets/views.py
+++ snippets/views.py
@@ -15,51 +15,33 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer,)
 
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-# @permission_classes((permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,IsAuthenticated, ))
-permission_classes((IsAuthenticated,permissions.IsAuthenticatedOrReadOnly, ))
-class SnippetList(generics.ListCreateAPIView):
-    # mixins.CreateModelMixin 可以保存数据
-    # generics.GenericAPIView 继承了APIView
+class SnippetViewSet(viewsets.ModelViewSet):
     """
-    # 这里是SnippetList接口描述
-    List all snippets, or create a new snippet.
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-# @permission_classes((permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,IsAuthenticated, ))
-permission_classes((permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,IsAuthenticated, ))
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    读取, 更新 or 删除一个代码片段(snippet)实例(instance).
+    This viewset automatically provides `list` and `detail` actions.
     """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
